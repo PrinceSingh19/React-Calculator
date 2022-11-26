33d2d9c7e842afa999f70e694942f8369ae3dcd1
Prince Singh <princesingh808087@gmail.com>
2022-11-26 21:25:01 +0530

    Added comments

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -7,22 +7,24 @@ export const ACTIONS = {
 };
 export const reducer = (state, { type, payload }) => {
 	switch (type) {
+		// implementing the digit add action
 		case ACTIONS.ADD_DIGIT:
 			if (state.overwrite) {
+				//checking the possibility to overwrite the digits
 				return {
 					...state,
 					currentVal: payload.digit,
 					overwrite: false,
 				};
 			}
-			if (payload.digit === "0" && state.currentVal === "0") return state;
-			if (payload.digit === "." && state.currentVal.includes(".")) return state;
+			if (payload.digit === "0" && state.currentVal === "0") return state; // making sure that 000 like situation do not occur
+			if (payload.digit === "." && state.currentVal.includes(".")) return state; //making sure that . is not added more than once
 			return {
 				...state,
 				currentVal: `${state.currentVal || ""}${payload.digit}`,
 			};
 
-		case ACTIONS.CHOOSE_OPERATION:
+		case ACTIONS.CHOOSE_OPERATION: //selecting the operation
 			if (state.currentVal === null && state.prevVal === null) {
 				return state;
 			}
@@ -48,17 +50,16 @@ export const reducer = (state, { type, payload }) => {
 				currentVal: null,
 			};
 
-		/* case ACTIONS.EVALUATE:
-      return evalate() */
-		case ACTIONS.CLEAR:
+		case ACTIONS.CLEAR: //clearing the display field
 			return {
 				...state,
 				currentVal: null,
 				prevVal: null,
 			};
 
-		case ACTIONS.EVALUATE:
+		case ACTIONS.EVALUATE: //implementing the evaluate feature
 			if (state.operation === null || state.currentVal === null || state.prevVal === null) {
+				//making sure that it do not implement until all the values are inplace
 				return state;
 			}
 			return {
@@ -69,8 +70,9 @@ export const reducer = (state, { type, payload }) => {
 				currentVal: evaluate(state),
 			};
 
-		case ACTIONS.DELETE_DIGIT:
+		case ACTIONS.DELETE_DIGIT: //deleting the values one by one
 			if (state.overwrite) {
+				//if value is after evaluating and we want to clear the fields after = then clear all
 				return {
 					...state,
 					overwrite: false,
@@ -79,6 +81,7 @@ export const reducer = (state, { type, payload }) => {
 			}
 			if (state.currentVal === null) return state;
 			if (state.currentVal.length === 1) {
+				//if one value the return null
 				return {
 					...state,
 					currentVal: null,
@@ -86,7 +89,7 @@ export const reducer = (state, { type, payload }) => {
 			}
 			return {
 				...state,
-				currentVal: state.currentVal.slice(0, -1),
+				currentVal: state.currentVal.slice(0, -1), //deleting the values one by one
 			};
 		default:
 			return {
@@ -96,10 +99,10 @@ export const reducer = (state, { type, payload }) => {
 };
 
 export function evaluate({ currentVal, prevVal, operation }) {
-	const prev = parseFloat(prevVal);
+	const prev = parseFloat(prevVal); //converting the string values into integer
 	const curr = parseFloat(currentVal);
 	let result = "";
-	if (isNaN(prev) || isNaN(curr)) return "";
+	if (isNaN(prev) || isNaN(curr)) return ""; //making sure that values are number
 	//eslint-disable-next-line
 	switch (operation) {
 		case "+":
@@ -115,5 +118,5 @@ export function evaluate({ currentVal, prevVal, operation }) {
 			result = prev / curr;
 			break;
 	}
-	return result.toString();
+	return result.toString(); //returning the values as string
 }
